d2fe9368f625facb535a3e591c5ad2d8e3c6c096
montasir <montasir1400@gmail.com>
2020-09-27 01:36:27 +0300

    написал функцию getRandomInt()

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+// const NUMBER_OF_MOCKS = 8;
+
+// The value is no lower min and is less than (but not equal to) max.
+const getRandomInt = (min = 0, max = 100) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+getRandomInt();
+
+// const createMock = () => ({
+
+// });
