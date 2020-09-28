21e78c258c10be015a2170ec36b1f333300ceb8e
montasir <montasir1400@gmail.com>
2020-09-28 20:11:51 +0300

    для createPost() добавил location

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 'use strict';
 
-const NUMBER_OF_MOCKS = 8;
-// const MIN_NUMBER_OF_USERS = 1;
+const NUMBER_OF_POSTS = 8;
+const MIM_NUMBER_OF_USERS = 1;
+// const POST_TITLES = [
+//   `Дворец`,
+//   `Квартира`,
+//   `Дом`,
+//   `Бунгало`
+// ];
+const STATIC_POINT_X = 600;
+const STATIC_POINT_Y = 350;
 
 // The value is no lower min and is less than (but not equal to) max.
 const getRandomInt = (min = 0, max = 100) => {
@@ -10,15 +18,31 @@ const getRandomInt = (min = 0, max = 100) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-getRandomInt();
+// getRandomInt();
 
-const createMock = (quantity = NUMBER_OF_MOCKS) => {
-  const mocksArray = [];
+const getLocationX = (r, x) => {
+  r = getRandomInt(-6, 6) * getRandomInt();
+  x = STATIC_POINT_X;
+  const LOCATION_X = x + r;
+
+  return LOCATION_X;
+};
+
+const getLocationY = (r, y) => {
+  r = getRandomInt(-3, 3) * getRandomInt();
+  y = STATIC_POINT_Y;
+  const LOCATION_Y = y + r;
+
+  return LOCATION_Y;
+};
+
+const createPost = (quantity = NUMBER_OF_POSTS) => {
+  const postsArray = [];
 
   for (let i = 0; i < quantity; i++) {
-    mocksArray.push({
+    postsArray.push({
       author: {
-        avatar: ``
+        avatar: `img/avatars/user0${getRandomInt(MIM_NUMBER_OF_USERS, NUMBER_OF_POSTS)}.png`
       },
       offer: {
         title: ``,
@@ -34,16 +58,16 @@ const createMock = (quantity = NUMBER_OF_MOCKS) => {
         photos: ``
       },
       location: {
-        x: ``,
-        y: ``
+        x: getLocationX(),
+        y: getLocationY()
       }
     });
   }
 
-  return mocksArray;
+  return postsArray;
 };
 
-createMock();
+createPost();
 
 const mapClass = document.querySelector(`.map`);
 mapClass.classList.remove(`map--faded`);
