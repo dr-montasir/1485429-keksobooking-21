8e025121eae8f63dfd781093b7d2f62c048ed204
montasir <montasir1400@gmail.com>
2020-09-28 16:53:14 +0300

    написал функцию для создания массива из 8 сгенерированных JS объектов

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
-// const NUMBER_OF_MOCKS = 8;
+const NUMBER_OF_MOCKS = 8;
+// const MIN_NUMBER_OF_USERS = 1;
 
 // The value is no lower min and is less than (but not equal to) max.
 const getRandomInt = (min = 0, max = 100) => {
@@ -11,9 +12,38 @@ const getRandomInt = (min = 0, max = 100) => {
 
 getRandomInt();
 
-// const createMock = () => ({
+const createMock = (quantity = NUMBER_OF_MOCKS) => {
+  const mocksArray = [];
 
-// });
+  for (let i = 0; i < quantity; i++) {
+    mocksArray.push({
+      author: {
+        avatar: ``
+      },
+      offer: {
+        title: ``,
+        address: ``,
+        price: ``,
+        type: ``,
+        rooms: ``,
+        guests: ``,
+        checkin: ``,
+        checkout: ``,
+        features: ``,
+        description: ``,
+        photos: ``
+      },
+      location: {
+        x: ``,
+        y: ``
+      }
+    });
+  }
+
+  return mocksArray;
+};
+
+createMock();
 
 const mapClass = document.querySelector(`.map`);
 mapClass.classList.remove(`map--faded`);
