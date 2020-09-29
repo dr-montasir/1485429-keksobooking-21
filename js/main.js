11bb616835eb0505eabcb55a44799a242641402f
montasir <montasir1400@gmail.com>
2020-09-29 07:07:06 +0300

    функция createPost завершена

--- js/main.js
+++ js/main.js
@@ -2,24 +2,55 @@
 
 const NUMBER_OF_POSTS = 8;
 const MIM_NUMBER_OF_USERS = 1;
-// const POST_TITLES = [
-//   `Дворец`,
-//   `Квартира`,
-//   `Дом`,
-//   `Бунгало`
-// ];
+const POST_TITLES = [`Дворец`, `Квартира`, `Дом`, `Бунгало`];
 const STATIC_POINT_X = 600;
 const STATIC_POINT_Y = 350;
 const PRICE_FROM = 500;
 const PRICE_TO = 10000;
-const NOT_FOR_GUESTS = 0;
-const NUMBER_OF_GUESTS = 4;
+const TYPE_OF_HOUSE = [`palace`, `flat`, `house`, `bungalow`];
+const NUMBER_OF_ROOMS = [1, 2, 3, 100];
+const NUMBER_OF_GUESTS = 3;
+const CHECKIN = [`12:00`, `13:00`, `14:00`];
+const CHECKOUT = [`12:00`, `13:00`, `14:00`];
+const FEATURES = [
+  `wifi`,
+  `dishwasher`,
+  `parking`,
+  `washer`,
+  `elevator`,
+  `conditioner`
+];
+const PHOTOS = [
+  `http://o0.github.io/assets/images/tokyo/hotel1.jpg`,
+  `http://o0.github.io/assets/images/tokyo/hotel2.jpg`,
+  `http://o0.github.io/assets/images/tokyo/hotel3.jpg`
+];
 
 // The value is no lower min and is less than (but not equal to) max.
 const getRandomInt = (min = 0, max = 100) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const getMaxIndex = (arr) => {
+  const maxIndex = arr.length - 1;
+
+  return maxIndex;
+};
+
+const minIndex = 0;
+
+const getTitleAndType = (arr1, arr2) => {
+  const arr = [];
+  arr1 = POST_TITLES;
+  arr2 = TYPE_OF_HOUSE;
+
+  for (let i = 0; i < 4; i++) {
+    arr.push({title: arr1[i], type: arr2[i]});
+  }
+
+  return arr;
 };
 
 const getLocationX = (x) => {
@@ -38,31 +69,45 @@ const getLocationY = (y) => {
   return LOCATION_Y;
 };
 
+const locationXY = {x: getLocationX(), y: getLocationY()};
+
+const createNumberOfGuests = (quantity = NUMBER_OF_GUESTS) => {
+  const arr = [];
+
+  for (let i = quantity; i >= 0; i--) {
+    arr.push(i);
+  }
+
+  return arr;
+};
+
+const notForGuests = createNumberOfGuests().slice(-1).pop();
+const guestsQuantity = getMaxIndex(createNumberOfGuests());
+
 const createPost = (quantity = NUMBER_OF_POSTS) => {
   const postsArray = [];
 
   for (let i = 0; i < quantity; i++) {
+    const titleAndType = getTitleAndType()[getRandomInt(minIndex, getMaxIndex(getTitleAndType()))];
+
     postsArray.push({
       author: {
         avatar: `img/avatars/user0${getRandomInt(MIM_NUMBER_OF_USERS, NUMBER_OF_POSTS)}.png`
       },
       offer: {
-        title: ``,
-        address: ``,
+        title: titleAndType.title,
+        address: `${locationXY.x}, ${locationXY.y}`,
         price: getRandomInt(PRICE_FROM, PRICE_TO),
-        type: ``,
-        rooms: ``,
-        guests: getRandomInt(NOT_FOR_GUESTS, NUMBER_OF_GUESTS),
-        checkin: ``,
-        checkout: ``,
-        features: ``,
-        description: ``,
-        photos: ``
+        type: titleAndType.type,
+        rooms: NUMBER_OF_ROOMS[getRandomInt(minIndex, getMaxIndex(NUMBER_OF_ROOMS))],
+        guests: getRandomInt(notForGuests, guestsQuantity),
+        checkin: CHECKIN[getRandomInt(minIndex, getMaxIndex(CHECKIN))],
+        checkout: CHECKOUT[getRandomInt(minIndex, getMaxIndex(CHECKOUT))],
+        features: FEATURES[getRandomInt(minIndex, getMaxIndex(FEATURES))],
+        description: `Описание`,
+        photos: PHOTOS[getRandomInt(minIndex, getMaxIndex(PHOTOS))]
       },
-      location: {
-        x: getLocationX(),
-        y: getLocationY()
-      }
+      location: locationXY
     });
   }
 
