c34419471abc4fdca85fe44801126a1102b1a7d7
montasir <montasir1400@gmail.com>
2020-10-25 20:24:28 +0300

    добавил const mapBlock в модуле popup.js

--- js/popup.js
+++ js/popup.js
@@ -1,6 +1,8 @@
 'use strict';
 
 (() => {
+  const mapBlock = document.querySelector(`.map`);
+
   const onPopupEscClose = (evt) => {
     if (evt.key === `Escape`) {
       onPopupClose();
@@ -8,13 +10,11 @@
   };
 
   const onPopupClose = () => {
-    // const mapCard = mapBlock.querySelector(`.map__card`);
-    const mapCard = document.querySelector(`.map__card`);
+    const mapCard = mapBlock.querySelector(`.map__card`);
 
     mapCard.remove();
 
-    // const activePin = mapBlock.querySelector(`.map__pin--active`);
-    const activePin = document.querySelector(`.map__pin--active`);
+    const activePin = mapBlock.querySelector(`.map__pin--active`);
 
     if (activePin) {
       activePin.classList.remove(`map__pin--active`);
