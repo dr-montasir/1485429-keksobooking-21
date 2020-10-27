c89af5e495ddc8c6c67a1296144a40193d87099c
montasir <montasir1400@gmail.com>
2020-10-27 23:19:18 +0300

    добавил timeout для функции downloadData в модуле load.js

--- js/load.js
+++ js/load.js
@@ -7,20 +7,31 @@
     OK: 200
   };
 
+  const TIMEOUT_IN_MS = 10000;
+
   const downloadData = (onSuccess, onError) => {
     const xhr = new XMLHttpRequest();
     xhr.responseType = `json`;
-
-    xhr.open(`GET`, receiverURL);
 
     xhr.addEventListener(`load`, () => {
       if (xhr.status === StatusCode.OK) {
         onSuccess(xhr.response);
       } else {
-        onError(`Произошла ошибка при загрузке данных`);
+        onError(`Статус ответа: ` + xhr.status + ` ` + xhr.statusText);
       }
     });
 
+    xhr.addEventListener(`error`, () => {
+      onError(`Произошла ошибка соединения`);
+    });
+
+    xhr.addEventListener(`timeout`, () => {
+      onError(`Запрос не успел выполниться за ` + xhr.timeout + ` мс`);
+    });
+
+    xhr.timeout = TIMEOUT_IN_MS;
+
+    xhr.open(`GET`, receiverURL);
     xhr.send();
   };
 
