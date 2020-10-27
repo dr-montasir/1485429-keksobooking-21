6ec821f5fdd5f86026501e1996eb2433e00bbf57
montasir <montasir1400@gmail.com>
2020-10-27 19:11:28 +0300

    добавил код модуля load.js -> функция downloadData

--- js/load.js
+++ js/load.js
@@ -1,5 +1,30 @@
 'use strict';
 
 (() => {
+  const receiverURL = `https://21.javascript.pages.academy/keksobooking/data`;
 
+  const StatusCode = {
+    OK: 200
+  };
+
+  const downloadData = (onSuccess, onError) => {
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = `json`;
+
+    xhr.open(`GET`, receiverURL);
+
+    xhr.addEventListener(`load`, () => {
+      if (xhr.status === StatusCode.OK) {
+        onSuccess(xhr.response);
+      } else {
+        onError(`Произошла ошибка при загрузке данных`);
+      }
+    });
+
+    xhr.send();
+  };
+
+  window.load = {
+    downloadData
+  };
 })();
