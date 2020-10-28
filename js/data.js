fda79b4854a6b42efe040fb48e99e851f39b67d6
montasir <montasir1400@gmail.com>
2020-10-28 03:43:07 +0300

    написал код модуля data.js

--- js/data.js
+++ js/data.js
@@ -1,77 +1,25 @@
 'use strict';
 
 (() => {
-  // const generateOffers = (quantity = window.consts.NUMBER_OF_OFFERS) => {
-  //   const offers = [];
+  const receiveOffersData = (receiveData, catchError) => {
 
-  //   for (let i = 0; i < quantity; i++) {
-  //     const house = window.consts.HOUSES[window.utils.getRandomInt(window.utils.minIndex, window.utils.getMaxIndex(window.consts.HOUSES))];
-  //     const locationXY = window.map.getlocationXY()[window.utils.getRandomInt(window.utils.minIndex, window.utils.getMaxIndex(window.map.getlocationXY()))];
+    receiveData = window.load.downloadData;
+    catchError = window.errors.errorHandler;
 
-  //     const generateOffer = () => {
-  //       return {
-  //         author: {
-  //           avatar: `img/avatars/user0${window.utils.getRandomInt(window.consts.MIM_NUMBER_OF_USERS, window.consts.NUMBER_OF_OFFERS)}.png`
-  //         },
-  //         offer: {
-  //           title: house.title,
-  //           address: `${locationXY.x}, ${locationXY.y}`,
-  //           price: window.utils.getRandomInt(window.consts.PRICE_FROM, window.consts.PRICE_TO),
-  //           type: house.type,
-  //           rooms: window.consts.NUMBER_OF_ROOMS[window.utils.getRandomInt(window.utils.minIndex, window.utils.getMaxIndex(window.consts.NUMBER_OF_ROOMS))],
-  //           guests: window.consts.NUMBER_OF_GUESTS[window.utils.getRandomInt(window.utils.minIndex, window.utils.getMaxIndex(window.consts.NUMBER_OF_GUESTS))],
-  //           checkin: window.consts.CHECKINS[window.utils.getRandomInt(window.utils.minIndex, window.utils.getMaxIndex(window.consts.CHECKINS))],
-  //           checkout: window.consts.CHECKOUTS[window.utils.getRandomInt(window.utils.minIndex, window.utils.getMaxIndex(window.consts.CHECKOUTS))],
-  //           features: window.utils.getRandomArray(window.consts.FEATURES),
-  //           description: `Описание`,
-  //           photos: window.utils.getRandomArray(window.consts.PHOTOS)
-  //         },
-  //         location: locationXY
-  //       };
-  //     };
-
-  //     offers.push(generateOffer());
-  //   }
-
-  //   return offers;
-  // };
-
-  // window.data = {generateOffers};
-
-  const receiveOffersData = (quantity = window.consts.NUMBER_OF_OFFERS) => {
     const offers = [];
 
-    for (let i = 0; i < quantity; i++) {
-      const house = window.consts.HOUSES[window.utils.getRandomInt(window.utils.minIndex, window.utils.getMaxIndex(window.consts.HOUSES))];
-      const locationXY = window.map.getlocationXY()[window.utils.getRandomInt(window.utils.minIndex, window.utils.getMaxIndex(window.map.getlocationXY()))];
+    receiveData((response) => {
 
-      const generateOffer = () => {
-        return {
-          author: {
-            avatar: `img/avatars/user0${window.utils.getRandomInt(window.consts.MIM_NUMBER_OF_USERS, window.consts.NUMBER_OF_OFFERS)}.png`
-          },
-          offer: {
-            title: house.title,
-            address: `${locationXY.x}, ${locationXY.y}`,
-            price: window.utils.getRandomInt(window.consts.PRICE_FROM, window.consts.PRICE_TO),
-            type: house.type,
-            rooms: window.consts.NUMBER_OF_ROOMS[window.utils.getRandomInt(window.utils.minIndex, window.utils.getMaxIndex(window.consts.NUMBER_OF_ROOMS))],
-            guests: window.consts.NUMBER_OF_GUESTS[window.utils.getRandomInt(window.utils.minIndex, window.utils.getMaxIndex(window.consts.NUMBER_OF_GUESTS))],
-            checkin: window.consts.CHECKINS[window.utils.getRandomInt(window.utils.minIndex, window.utils.getMaxIndex(window.consts.CHECKINS))],
-            checkout: window.consts.CHECKOUTS[window.utils.getRandomInt(window.utils.minIndex, window.utils.getMaxIndex(window.consts.CHECKOUTS))],
-            features: window.utils.getRandomArray(window.consts.FEATURES),
-            description: `Описание`,
-            photos: window.utils.getRandomArray(window.consts.PHOTOS)
-          },
-          location: locationXY
-        };
-      };
+      response.map((object) => {
+        offers.push(object);
+      });
 
-      offers.push(generateOffer());
-    }
+    }, catchError);
 
     return offers;
   };
 
-  window.data = {receiveOffersData};
+  window.data = {
+    receiveOffersData
+  };
 })();
