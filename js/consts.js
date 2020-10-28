f407b7e317f87cfeb7e0c6c36830c8ac93a024cc
montasir <montasir1400@gmail.com>
2020-10-29 02:38:26 +0300

    правки по module6-task1

--- js/consts.js
+++ js/consts.js
@@ -1,7 +1,6 @@
 'use strict';
 
 (() => {
-  const NUMBER_OF_OFFERS = 8;
   const STATIC_POINTS = Object.freeze({x1: 0, x2: 1200, y1: 130, y2: 630});
   const XY_OFFSET = Object.freeze({x: 25, y: 50});
   const FORM_TITLE_LENGTH = Object.freeze({min: 30, max: 100});
@@ -14,7 +13,6 @@
   const HIGHT_MAINPIN_TAIL = 15;
 
   window.consts = {
-    NUMBER_OF_OFFERS,
     STATIC_POINTS,
     XY_OFFSET,
     FORM_TITLE_LENGTH,
