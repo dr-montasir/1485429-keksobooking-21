7a86b5d48550221e0465f1e3b9cc768a993abe84
montasir <montasir1400@gmail.com>
2020-11-03 14:54:29 +0300

    изменил название модуля fliteration на filters

---
+++ js/filters.js
@@ -0,0 +1,7 @@
+'use strict';
+
+(() => {
+  // const mapFilters = document.querySelector(`.map__filters`);
+  // const mapFilter = mapFilters.querySelectorAll(`.map__filter`);
+  // const housingType = mapFilters.querySelector(`#housing-type`);
+})();
