f53952131317f5c53c1dced3d125c95f3ae6517d
montasir <montasir1400@gmail.com>
2020-11-08 02:32:31 +0300

    добавил package webpack и точка входа (entry)

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,15 @@
 module.exports = {
-
+  entry: [
+    "./js/constants.js",
+    "./js/debounce.js",
+    "./js/load.js",
+    "./js/filters.js",
+    "./js/dialog.js",
+    "./js/map.js",
+    "./js/pin.js",
+    "./js/move.js",
+    "./js/popup.js",
+    "./js/form.js",
+    "./js/main.js"
+  ]
 };
